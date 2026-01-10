cf86e7987f78e240e964ae91454aee1547188718
RaviinduSomassiri <ravindusomasiri0@gmail.com>
2026-01-10 20:07:13 +0530

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// This connects the website to the XAMPP database
-$conn = mysqli_connect("localhost:3307", "root", "", "drivehub_db");
-
-if (!$conn) {
-    die("Connection failed!");
-}
-?>
